600f098e42cd8e30fefc8f2521ed23f36b2a4299
maib ecommerce <117263659+maib-ecomm@users.noreply.github.com>
2023-06-20 23:57:04 +0300

    Update callbackUrl.php refactoring

--- examples/callbackUrl.php
+++ examples/callbackUrl.php
@@ -8,44 +8,10 @@ $data = json_decode($json, true);
 if (isset($data['signature'])) {
     $data_result = $data['result']; // Extract data from the "result" object
 
-    /**
-     * Sorts an array by keys recursively.
-     *
-     * @param array $array The array to be sorted.
-     * @return array The sorted array.
-     */
-    function sortByKeyRecursive(array $array)
-    {
-        ksort($array, SORT_STRING);
-        foreach ($array as $key => &$value) {
-            if (is_array($value)) {
-                $value = sortByKeyRecursive($value);
-            }
-        }
-        unset($value); // Unset the reference variable to avoid potential issues
-        return $array;
-    }
-
-    /**
-     * Implode an array recursively with a separator.
-     *
-     * @param string $separator The separator string.
-     * @param array $array The array to be imploded.
-     * @return string The imploded string.
-     */
-    function implodeRecursive($separator, $array)
-    {
-        $result = '';
-        foreach ($array as $item) {
-            $result .= (is_array($item) ? implodeRecursive($separator, $item) : (string)$item) . $separator;
-        }
-        return substr($result, 0, -1);
-    }
-
     $sortedDataByKeys = sortByKeyRecursive($data_result); // Sort the data array by keys recursively
     $sortedDataByKeys[] = SIGNATURE_KEY; // Add the Signature Key to the end of the data array
     $signString = implodeRecursive(':', $sortedDataByKeys); // Implode the sorted array recursively
-    $sign = base64_encode(hash('sha256', $signString, true)); // Generate the signature
+    $sign = generateSignature($signString); // Generate the signature
 
     // Compare the generated signature with the received signature on the Callback URL
     if ($sign === $data['signature']) {
@@ -55,4 +21,49 @@ if (isset($data['signature'])) {
         // Signature is invalid, reject the request
         // ...
     }
+}
+
+/**
+ * Sorts an array by keys recursively.
+ *
+ * @param array $array The array to be sorted.
+ * @return array The sorted array.
+ */
+function sortByKeyRecursive(array $array)
+{
+    ksort($array, SORT_STRING);
+    foreach ($array as $key => &$value) {
+        if (is_array($value)) {
+            $value = sortByKeyRecursive($value);
+        }
+    }
+    unset($value); // Unset the reference variable to avoid potential issues
+    return $array;
+}
+
+/**
+ * Implode an array recursively with a separator.
+ *
+ * @param string $separator The separator string.
+ * @param array $array The array to be imploded.
+ * @return string The imploded string.
+ */
+function implodeRecursive($separator, $array)
+{
+    $result = '';
+    foreach ($array as $item) {
+        $result .= (is_array($item) ? implodeRecursive($separator, $item) : (string)$item) . $separator;
+    }
+    return substr($result, 0, -1);
+}
+
+/**
+ * Generate the signature for the given string.
+ *
+ * @param string $string The string to generate the signature from.
+ * @return string The generated signature.
+ */
+function generateSignature($string)
+{
+    return base64_encode(hash('sha256', $string, true));
 }
