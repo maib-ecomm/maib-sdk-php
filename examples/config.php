1afc8c1ee5b0c8f8a4b9b6b626332c7bff66dea0
maib ecommerce <117263659+maib-ecomm@users.noreply.github.com>
2023-07-03 18:28:41 +0300

    Update config.php

--- examples/config.php
+++ examples/config.php
@@ -2,8 +2,9 @@
 require __DIR__ . '/../src/MaibAuthRequest.php';
 require __DIR__ . '/../src/MaibApiRequest.php';
 require __DIR__ . '/../src/MaibSdk.php';
-class_alias("MaibEcomm\MaibSdk\MaibAuthRequest", "MaibAuthRequest");
-class_alias("MaibEcomm\MaibSdk\MaibApiRequest", "MaibApiRequest");
+
+use MaibEcomm\MaibSdk\MaibAuthRequest;
+use MaibEcomm\MaibSdk\MaibApiRequest;
 
 // Project settings from maibmerchants.md
 // Project Secret and Signature Key are available after Project activation
