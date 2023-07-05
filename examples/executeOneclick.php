cb8d09e3f1621091cd85f0f16b78087bb94561c1
maib ecommerce <117263659+maib-ecomm@users.noreply.github.com>
2023-07-05 11:23:04 +0300

    Update executeOneclick.php

--- examples/executeOneclick.php
+++ examples/executeOneclick.php
@@ -25,7 +25,7 @@ $data = array(
     ),
     'callbackUrl' => 'https://example.com/callback',
     'okUrl' => 'https://example.com/ok',
-    'failUrl' => 'https://examplecom/fail'
+    'failUrl' => 'https://example.com/fail'
 );
 
 // Initiate execute One-click Payment
